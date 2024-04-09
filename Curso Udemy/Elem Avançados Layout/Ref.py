b586d413182645cd5bce9b354d52545050f6b3eb
Kaue Santana <kauesantana_13@hotmail.com>
2024-04-09 20:36:11 -0300

    Elemento Avançados

--- Curso Udemy/Elem Avançados Layout/Ref.py
+++ Curso Udemy/Elem Avançados Layout/Ref.py
@@ -1,13 +1,86 @@
-# Importe a biblioteca flet com o alias ft
 import flet as ft
 
-# flet -r Ref.py
-# Defina a função principal que será executada no aplicativo
 def main(page: ft.Page):
+    layout = ft.Container(
+        bgcolor=ft.colors.AMBER,
+        padding=ft.padding.all(20),
+        content=ft.Row(
+            controls=[
+                ft.Container(
+                    bgcolor=ft.colors.RED,
+                    padding=ft.padding.all(20),
+                    content=ft.Icon(name=ft.icons.ABC, size=30)
+                ),
+                ft.Container(
+                    bgcolor=ft.colors.CYAN,
+                    padding=ft.padding.all(20),
+                    content=ft.Column(
+                        controls=[
+                            ft.Text(value='Título do elemento', weight=ft.FontWeight.BOLD),
+                            ft.Text(value='Conteúdo do componente com informações importantes')
+                        ]
+                    )
+                ),
+            ]
+        )
+    )
 
-    ...
+    page.add(layout)
 
-# Verifica se este script está sendo executado diretamente
+    # Problema que o Ref resolve
+    # layout.content.controls[0].content.name = ft.icons.PHONE
+    # layout.content.controls[0].content.update()
+
+    # layout.content.controls[1].content.controls[0].value = 'Novo título'
+    # layout.content.update()
+
+
+    # Solução com Ref
+    icone_componente = ft.Ref[ft.Icon]()
+    titulo_componente = ft.Ref[ft.Text]()
+
+
+    layout = ft.Container(
+        bgcolor=ft.colors.AMBER,
+        padding=ft.padding.all(20),
+        content=ft.Row(
+            controls=[
+                ft.Container(
+                    bgcolor=ft.colors.RED,
+                    padding=ft.padding.all(20),
+                    content=ft.Icon(
+                        ref=icone_componente,
+                        name=ft.icons.ABC,
+                        size=30
+                    )
+                ),
+                ft.Container(
+                    bgcolor=ft.colors.CYAN,
+                    padding=ft.padding.all(20),
+                    content=ft.Column(
+                        controls=[
+                            ft.Text(
+                                ref=titulo_componente,
+                                value='Título do elemento',
+                                weight=ft.FontWeight.BOLD
+                            ),
+                            ft.Text(value='Conteúdo do componente com informações importantes')
+                        ]
+                    )
+                ),
+            ]
+        )
+    )
+
+    page.add(layout)
+
+    icone_componente.current.name = ft.icons.PHONE
+    icone_componente.current.update()
+
+    titulo_componente.current.value = 'Novo título'
+    titulo_componente.current.update()
+
+
+
 if __name__ == '__main__':
-    # Abre a página da web e executa a função principal nela
     ft.app(target=main)
--- Curso Udemy/Elem Avançados Layout/Tooltip.py
+++ Curso Udemy/Elem Avançados Layout/Tooltip.py
@@ -1,11 +1,32 @@
 # Importe a biblioteca flet com o alias ft
 import flet as ft
 
-# flet -r Tooltip.py
+# flet -r Tooltip
+# .py
 # Defina a função principal que será executada no aplicativo
 def main(page: ft.Page):
 
-    ...
+    tp = ft.Tooltip(
+        message="Tooltip",
+        content=ft.Text(value="Passe o mouse aqui"),
+        bgcolor=ft.colors.BLACK,
+        border=ft.border.all(width=5, color=ft.colors.WHITE),
+        border_radius=ft.border_radius.all(5),
+        height=100,
+        margin=ft.margin.all(20),
+        padding=ft.padding.all(10),
+        text_align=ft.TextAlign.CENTER,
+        text_style=ft.TextStyle(color=ft.colors.WHITE, italic=True, size=30),
+        wait_duration=1000,
+        gradient=ft.LinearGradient(
+            begin=ft.alignment.bottom_right,
+            end=ft.alignment.top_right,
+            colors=[ft.colors.CYAN, ft.colors.TEAL]
+        )
+
+    )
+
+    page.add(tp)
 
 # Verifica se este script está sendo executado diretamente
 if __name__ == '__main__':
