97ff82b224df6712a4f2c0ebe52e376c96f95d20
Kaue Santana <kauesantana_13@hotmail.com>
2024-04-09 20:40:47 -0300

    Projeto ToDo comentado

--- Curso Udemy/Projetos Práticos/ToDo/todo.py
+++ Curso Udemy/Projetos Práticos/ToDo/todo.py
@@ -1,10 +1,14 @@
-import flet as ft
-import sqlite3
+import flet as ft  # Importa a biblioteca flet com o alias ft
+import sqlite3  # Importa o módulo sqlite3 para interagir com o banco de dados SQLite
+
 
 # flet -r todo.py
 
+# Definição da classe Todo
 class Todo:
+    # Método construtor da classe
     def __init__(self, page: ft.Page):
+        # Inicialização de variáveis e configuração da página
         self.page = page
         self.page.bgcolor = ft.colors.BLACK
         self.page.window_width = 350
@@ -14,46 +18,51 @@ class Todo:
         self.page.title = "ToDo App"
         self.task = ''
         self.view = "all"
+
+        # Criação da tabela 'tasks' se não existir no banco de dados
         self.db_execute("CREATE TABLE IF NOT EXISTS tasks(name, status)")
+        # Consulta inicial para obter todas as tarefas
         self.results = self.db_execute("SELECT * FROM tasks")
 
-
+        # Chamada para criar a página principal
         self.main_page()
 
-    def db_execute(self, query, params = []):
+    # Método para executar consultas no banco de dados
+    def db_execute(self, query, params=[]):
         with sqlite3.connect("database.db") as con:
             cur = con.cursor()
             cur.execute(query, params)
             con.commit()
             return cur.fetchall()
 
+    # Método chamado quando a checkbox de uma tarefa é marcada ou desmarcada
     def checked(self, e):
         is_checked = e.control.value
         label = e.control.label
 
+        # Atualiza o status da tarefa no banco de dados com base na marcação da checkbox
         if is_checked:
             self.db_execute('UPDATE tasks SET status = "complete" WHERE name = ?', params=[label])
         else:
             self.db_execute('UPDATE tasks SET status = "incomplete" WHERE name = ?', params=[label])
-            
-        
+
+        # Atualiza a lista de tarefas com base na visualização selecionada
         if self.view == "all":
             self.results = self.db_execute("SELECT * FROM tasks")
         else:
             self.results = self.db_execute("SELECT * FROM tasks WHERE status =?", params=[self.view])
-            
-            
 
+        # Atualiza a lista de tarefas na página
         self.update_task_list()
 
+    # Método para criar o contêiner das tarefas
     def tasks_container(self):
         return ft.Container(
             height=self.page.height * 0.8,
             content=ft.Column(
                 controls=[
-
                     ft.Checkbox(
-                        on_change= self.checked,
+                        on_change=self.checked,
                         label=res[0],
                         value=True if res[1] == "complete" else False,
                     )
@@ -62,27 +71,23 @@ class Todo:
             )
         )
 
+    # Método chamado quando o valor do campo de entrada é alterado
     def set_value(self, e):
         self.task = e.control.value
-        print(self.task)
 
+    # Método chamado para adicionar uma nova tarefa
     def add(self, e, input_task):
         name = self.task
         status = "incomplete"
 
+        # Insere a nova tarefa no banco de dados e limpa o campo de entrada
         if name:
             self.db_execute(query="INSERT INTO tasks VALUES(?, ?)", params=[name, status])
             input_task.value = ""
             self.results = self.db_execute("SELECT * FROM tasks")
             self.update_task_list()
-        
-    def update_task_list(self):
-        tasks = self.tasks_container()
-        self.page.controls.pop()  # Remove old task list
-        self.page.add(tasks)     # Add updated task list
-        self.page.update()
-        
-    
+
+    # Método chamado quando o usuário muda de guia (todos, em andamento, finalizados)
     def tabs_changed(self, e):
         if e.control.selected_index == 0:
             self.results = self.db_execute("SELECT * FROM tasks")
@@ -93,11 +98,19 @@ class Todo:
         elif e.control.selected_index == 2:
             self.results = self.db_execute('SELECT * FROM tasks WHERE status = "complete" ')
             self.view = "complete"
-            
-        self.update_task_list()
-        
 
+        self.update_task_list()
+
+    # Método para atualizar a lista de tarefas na página
+    def update_task_list(self):
+        tasks = self.tasks_container()
+        self.page.controls.pop()  # Remove a lista de tarefas antiga
+        self.page.add(tasks)  # Adiciona a lista de tarefas atualizada
+        self.page.update()
+
+    # Método para criar a página principal
     def main_page(self):
+        # Campo de entrada para adicionar novas tarefas
         input_task = ft.TextField(
             hint_text="Digite uma tarefa",
             expand=True,
@@ -105,6 +118,7 @@ class Todo:
             on_change=self.set_value,
         )
 
+        # Barra de entrada com o campo de texto e o botão de adicionar tarefa
         input_bar = ft.Row(
             controls=[
                 input_task,
@@ -115,24 +129,23 @@ class Todo:
             ]
         )
 
+        # Guias para selecionar a visualização das tarefas
         tabs = ft.Tabs(
             selected_index=0,
-            on_change = self.tabs_changed,
+            on_change=self.tabs_changed,
             tabs=[
                 ft.Tab(text="Todos"),
                 ft.Tab(text="Em andamento"),
                 ft.Tab(text="Finalizados"),
             ]
         )
-        
-        
 
+        # Contêiner para exibir as tarefas
         tasks = self.tasks_container()
 
+        # Adiciona os controles à página
         self.page.add(input_bar, tabs, tasks)
 
 
-
-
-
-ft.app(target= Todo)
+# Executa o aplicativo com a classe Todo como alvo
+ft.app(target=Todo)
